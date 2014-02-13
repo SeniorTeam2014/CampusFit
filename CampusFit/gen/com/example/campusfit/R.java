fdaf80d0cab67542a803d6f8dcdb3a2265662617
ryan bennett <bennet19@purdue.edu>
2014-02-12 22:34:33 -0500

    profile page gui

--- CampusFit/gen/com/example/campusfit/R.java
+++ CampusFit/gen/com/example/campusfit/R.java
@@ -24,34 +24,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int action_settings=0x7f08000a;
-=======
         public static final int Button01=0x7f08000a;
         public static final int Button02=0x7f080007;
         public static final int Button03=0x7f080008;
         public static final int Button04=0x7f080009;
-        public static final int action_settings=0x7f08000b;
->>>>>>> 25a692441bbd1abae6a486e31c7aba62a0802795
+        public static final int action_settings=0x7f080016;
         public static final int button1=0x7f080005;
         public static final int button2=0x7f080006;
         public static final int editText1=0x7f080000;
         public static final int editText2=0x7f080004;
-        public static final int editText3=0x7f080009;
+        public static final int editText3=0x7f080013;
+        public static final int editText4=0x7f080014;
+        public static final int radio0=0x7f080010;
+        public static final int radio1=0x7f080011;
+        public static final int radioGroup1=0x7f08000d;
+        public static final int radioGroup2=0x7f08000e;
         public static final int textView1=0x7f080003;
         public static final int textView2=0x7f080001;
         public static final int textView3=0x7f080002;
-        public static final int textView4=0x7f080008;
-        public static final int toggleButton1=0x7f080007;
+        public static final int textView4=0x7f08000c;
+        public static final int textView5=0x7f08000f;
+        public static final int textView6=0x7f080012;
+        public static final int textView7=0x7f080015;
+        public static final int toggleButton1=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-<<<<<<< HEAD
-        public static final int profile=0x7f030001;
-        public static final int sign_up=0x7f030002;
-=======
         public static final int menu=0x7f030001;
->>>>>>> 25a692441bbd1abae6a486e31c7aba62a0802795
+        public static final int profile=0x7f030002;
+        public static final int sign_up=0x7f030003;
+        public static final int workout=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- CampusFit/src/com/example/campusfit/MainActivity.java
+++ CampusFit/src/com/example/campusfit/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.profile);
+		setContentView(R.layout.workout);
 	}
 
 	@Override
