ac7b51a55f383c3486c57544753b17d9e1bf75d9
Ryan Bennett <bennet19@purdue.edu>
2014-03-31 12:17:20 -0400

    minor changes

--- CampusFit/src/com/example/campusfit/MatchmakerList.java
+++ CampusFit/src/com/example/campusfit/MatchmakerList.java
@@ -1,5 +1,8 @@
 package com.example.campusfit;
 
+import java.util.ArrayList;
+
+import android.app.ActionBar;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,27 +18,47 @@ public class MatchmakerList extends ListActivity {
 	static final String[] list = new String[] { "Goals", "Interests", "Time",
 			"Gender", "Age" };
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.matchmaker, list));
-		final ListView listView = getListView();
-		listView.setTextFilterEnabled(true);
-		listView.setClickable(true);
-		listView.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-
-				Toast.makeText(getApplicationContext(),
-						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-					
-				
-			}
-
-		});
-
+	
+	ArrayList<String> val = new ArrayList<String>();
+	ArrayAdapter<String> adapter;
+	
+	public void setActionBar(String heading) {
+		ActionBar actionBar = getActionBar();
+		actionBar.setTitle(heading);
+		actionBar.show();
 	}
+	
+	@Override
+	public void onCreate(Bundle i) {
+		setActionBar("Match Maker");
+		super.onCreate(i);
+		val.add("Goals");
+		val.add("Interests");
+		val.add("Time");
+		val.add("Gender");
+		val.add("Age");
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, val);
+		setListAdapter(adapter);
+		final ListView lv = getListView();
+		lv.setOnItemClickListener( new AdapterView.OnItemClickListener() { 
+		    @Override 
+		    public void onItemClick( AdapterView<?> av, View v, int pos, long id ) { 
+		              onListItemClick(lv, v, pos, id); 
+		              //return false; 
+		    } 
+		} );
+		
+	}
+	
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		String item = (String) getListAdapter().getItem(position);
+		Toast.makeText(this, item, Toast.LENGTH_LONG).show();
+	}
+	
+	
+
 
 }
